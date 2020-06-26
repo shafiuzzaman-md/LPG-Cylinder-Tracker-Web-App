569ce4b0a65965db2883f607c5d5f08f2c383b61
Mostafijur Rahman <40955844+MostafijurJ@users.noreply.github.com>
2020-06-26 23:16:58 +0600

    files uploading

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func apiResponse(w http.ResponseWriter, r *http.Request) {
@@ -24,8 +28,57 @@ func apiResponse(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type info struct {
+	Offers       string
+	Select_Users string
+	Open_Date    string
+	Close_Date   string
+	Checked      string
+}
+
 func main() {
-	fmt.Println("Hello")
 	http.HandleFunc("/users", apiResponse)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/temp", func(w http.ResponseWriter, req *http.Request) {
+
+		http.ServeFile(w, req, "Template/temp.html")
+	})
+
+	tmpl := template.Must(template.ParseFiles("../temp.html"))
+	db, err := sql.Open("mysql", "kajol:kajol123@(192.168.43.140:3306)/Cylinder_tracking")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		qu := "select Offers, Select_Users, Open_Date, Close_Date, Checked from offer"
+
+		rows, err := db.Query(qu)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer rows.Close()
+
+		var informations []info
+		for rows.Next() {
+			var temp info
+			err = rows.Scan(&temp.Offers, &temp.Select_Users, &temp.Open_Date, &temp.Close_Date, &temp.Checked)
+
+			informations = append(informations, temp)
+
+		}
+		fmt.Println(informations)
+
+		tmpl.Execute(w, struct{ Informations []info }{informations})
+	})
+
+	log.Fatal(http.ListenAndServe(":8090", nil))
 }
